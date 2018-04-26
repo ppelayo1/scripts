a6941ee3db86550e257d75f9ff9bc5bbc9bfe7c3
ppelayo1 <32974267+ppelayo1@users.noreply.github.com>
2018-04-25 18:30:41 -0700

    Getting basic search working

--- Project v2/PHP/searchDB.php
+++ Project v2/PHP/searchDB.php
@@ -2,8 +2,41 @@
 
 	//variables
 	$q = $_REQUEST["q"];
+	$strn = ''; //This string will hold the querry request
+	$result = '';    //Results of the querry
 
-	echo $q;
+	//Login information
+    $hostname = "localhost"; // the hostname you created when creating the database
+    $username = "root";      // the username specified when setting up the database
+    $password = "";      // the password specified when setting up the database
+    $database = "starwarsapi";      // the database name chosen when setting up the database 
+    
+
+    //connect to the database
+    $con = new mysqli($hostname,$username,$password,$database);
+
+    
+    //build the querry string
+    $strn = 'SELECT Name FROM People
+    			WHERE Name LIKE "' . $q . '%"';
+
+
+
+    //Connect and get results
+    $results = $con->query($strn);
+
+    
+
+
+    //print out the matches
+    while($row = $results->fetch_assoc())
+    	echo $row["Name"] . "<br>";
+    	
+
+    
+
+
+	
 
 
 ?>
