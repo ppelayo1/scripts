a544d951a9d39bbeb0c636b9007263bedf15ba06
ppelayo1 <32974267+ppelayo1@users.noreply.github.com>
2018-04-25 20:14:33 -0700

    working on the css search bar

--- Project v2/PHP/searchDB.php
+++ Project v2/PHP/searchDB.php
@@ -5,36 +5,39 @@
 	$strn = ''; //This string will hold the querry request
 	$result = '';    //Results of the querry
 
-	//Login information
-    $hostname = "localhost"; // the hostname you created when creating the database
-    $username = "root";      // the username specified when setting up the database
-    $password = "";      // the password specified when setting up the database
-    $database = "starwarsapi";      // the database name chosen when setting up the database 
+		//only perform the steps if a value exists
+		if($q != ""){
+
+		//Login information
+	    $hostname = "localhost"; // the hostname you created when creating the database
+	    $username = "root";      // the username specified when setting up the database
+	    $password = "";      // the password specified when setting up the database
+	    $database = "starwarsapi";      // the database name chosen when setting up the database 
+	    
+
+	    //connect to the database
+	    $con = new mysqli($hostname,$username,$password,$database);
+
+	    
+	    //build the querry string
+	    $strn = 'SELECT Name FROM People
+	    			WHERE Name LIKE "' . $q . '%"';
+
+
+
+	    //Connect and get results
+	    $results = $con->query($strn);
+
+	    
+
+
+	    //print out the matches
+	    while($row = $results->fetch_assoc())
+	    	echo $row["Name"] . "<br>";
+	    	
+
     
-
-    //connect to the database
-    $con = new mysqli($hostname,$username,$password,$database);
-
-    
-    //build the querry string
-    $strn = 'SELECT Name FROM People
-    			WHERE Name LIKE "' . $q . '%"';
-
-
-
-    //Connect and get results
-    $results = $con->query($strn);
-
-    
-
-
-    //print out the matches
-    while($row = $results->fetch_assoc())
-    	echo $row["Name"] . "<br>";
-    	
-
-    
-
+	}
 
 	
 
